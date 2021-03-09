a2a23390c66a8c25cfa1314f4109339324b40e28
Junior Ferreira <web2ajax@gmail.com>
2021-03-09 18:20:36 -0300

    first commit

---
+++ Assets/src/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance;
+
+    private void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName) {
+        StartCoroutine(ShowOverLayAndLoad(sceneName));
+    }
+
+    IEnumerator ShowOverLayAndLoad(string sceneName) {
+        yield return new WaitForSeconds(3f);
+
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+        yield return null;
+    }
+}
